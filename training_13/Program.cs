b71a21a1a66aaad2a2a9a0495b45fd304fcd77cf
agbeloborodov <neosatum@yandex.ru>
2022-07-14 19:36:31 +0300

    Задача №13

---
+++ training_13/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        // z.B.:  645 -> 5
+        // z.B.:  78 -> третьей цифры нет
+        // z.B.:  32679 -> 6
+        Console.Write("Введите любое число: ");
+        string array_of_numbers = Console.ReadLine();
+        if (array_of_numbers.Length < 3)
+            Console.WriteLine("В введённом Вами числе нет третьей цифры");
+        else
+            Console.WriteLine("Третья цифра заданного числа: " + array_of_numbers[2]);
+    }
+}
