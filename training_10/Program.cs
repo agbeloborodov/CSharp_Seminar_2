ce6b4ea6bc834b9cc5212cb253ef30658e5391a1
agbeloborodov <neosatum@yandex.ru>
2022-07-14 19:36:10 +0300

    Задача №10

---
+++ training_10/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+        int SecondDigit(int arg1)
+        {
+            int sec_dig = arg1 / 10 % 10;
+            return sec_dig;
+        }
+        Console.Write("Введите трёхзначное число: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Вторая цифра введённого числа:  " + SecondDigit(a));
+    }
+}
